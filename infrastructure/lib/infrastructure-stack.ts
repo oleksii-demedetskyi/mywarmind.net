44376c8d59f60bbc31b48cc078639c597d67b27a
Andrii Moskvin <berkold90@gmail.com>
2020-03-19 09:22:56 +0200

    Updated layers to correct links

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -11,8 +11,8 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const kLayers = LayerVersion.fromLayerVersionArn(this, 'k-layers', 'arn:aws:lambda:us-east-2:113088814899:layer:Klayers-python37-requests:8');
-    const xRay = LayerVersion.fromLayerVersionArn(this, 'x-ray', 'arn:aws:lambda:us-east-2:113088814899:layer:Klayers-python37-aws-xray-sdk:15');
+    const kLayers = LayerVersion.fromLayerVersionArn(this, 'k-layers', 'arn:aws:lambda:us-east-2:770693421928:layer:Klayers-python38-requests:2');
+    const xRay = LayerVersion.fromLayerVersionArn(this, 'x-ray', 'arn:aws:lambda:us-east-2:770693421928:layer:Klayers-python38-aws-xray-sdk:14');
     const layers = [kLayers, xRay]
 
     const api = new apiGateway.RestApi(this, 'warmind-gateway', { restApiName: 'mywarmind' });
