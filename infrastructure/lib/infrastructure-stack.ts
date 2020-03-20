d134178f1514fd7ce65cc1560a990d53a2e32617
Andrii Moskvin <berkold90@gmail.com>
2020-03-20 14:11:52 +0200

    Added work with s3 bucket

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -8,6 +8,9 @@ import weapons from "./weapons";
 import updateManifest from "./update-manifest"
 import { AwsIntegration } from "@aws-cdk/aws-apigateway";
 import * as iam from '@aws-cdk/aws-iam'
+import { Bucket } from "@aws-cdk/aws-s3";
+import * as s3Deploy from '@aws-cdk/aws-s3-deployment'
+import { ServicePrincipal } from "@aws-cdk/aws-iam";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,7 +21,14 @@ export class InfrastructureStack extends cdk.Stack {
     const layers = [kLayers, xRay]
 
     const api = new apiGateway.RestApi(this, 'warmind-gateway', { restApiName: 'infra-mywarmind' });
-    const role = iam.Role.fromRoleArn(this, 'ApiGateway-read-mywarmind-s3', 'arn:aws:iam::956931160472:role/ApiGateway-read-mywarmind-s3', { mutable: false });
+    const bucket = new Bucket(this, 'infra-mywarmind')
+    new s3Deploy.BucketDeployment(this, 'infra-mywarmind-bucket-deploy', {
+      sources: [s3Deploy.Source.asset('../public')],
+      destinationBucket: bucket
+    })
+    const role = new iam.Role(this, 'infra-mywarmind-read-s3', { assumedBy: new ServicePrincipal('s3')})
+    bucket.grantRead(role)
+    // const role = iam.Role.fromRoleArn(this, 'ApiGateway-read-mywarmind-s3', 'arn:aws:iam::956931160472:role/ApiGateway-read-mywarmind-s3', { mutable: false });
     const s3Integration = new AwsIntegration( { service: 's3', path: 'mywarmind.net/index.html', options: { credentialsRole: role }})
     api.root.addMethod('GET', s3Integration)
 
