9e53b93544fa9b87296318aba434aa95ebdfde5b
Andrii Moskvin <berkold90@gmail.com>
2020-03-25 09:11:07 +0200

    Enabled bucket deployment

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -21,15 +21,16 @@ export class InfrastructureStack extends cdk.Stack {
     const layers = [kLayers, xRay]
 
     const api = new apiGateway.RestApi(this, 'warmind-gateway', { restApiName: 'infra-mywarmind' });
+
     const bucket = new Bucket(this, 'infra-mywarmind')
-    // temporary disabled
-    // new s3Deploy.BucketDeployment(this, 'infra-mywarmind-bucket-deploy', {
-    //   sources: [s3Deploy.Source.asset('../public')],
-    //   destinationBucket: bucket
-    // })
+    new s3Deploy.BucketDeployment(this, 'infra-mywarmind-bucket-deploy', {
+      sources: [s3Deploy.Source.asset('../public')],
+      destinationBucket: bucket
+    })
+
     const role = new iam.Role(this, 'infra-mywarmind-read-s3', { assumedBy: new ServicePrincipal('s3')})
     bucket.grantRead(role)
-    // const role = iam.Role.fromRoleArn(this, 'ApiGateway-read-mywarmind-s3', 'arn:aws:iam::956931160472:role/ApiGateway-read-mywarmind-s3', { mutable: false });
+
     const s3Integration = new AwsIntegration( { service: 's3', path: 'mywarmind.net/index.html', options: { credentialsRole: role }})
     api.root.addMethod('GET', s3Integration)
 
