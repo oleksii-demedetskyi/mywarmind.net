73e2b46addc3fcfef6baed6b6b78aa1154f8defc
Andrii Moskvin <andrii.moskvin@gmail.com>
2020-03-31 20:08:35 +0300

    Still not working S3 integration and passthrough is YES!!!!

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import auth from './auth'
 import * as apiGateway from "@aws-cdk/aws-apigateway";
-import { AwsIntegration, EndpointType, PassthroughBehavior } from "@aws-cdk/aws-apigateway";
+import { AwsIntegration, ContentHandling, EndpointType, PassthroughBehavior } from "@aws-cdk/aws-apigateway";
 import gear from "./gear";
 import { LayerVersion } from "@aws-cdk/aws-lambda";
 import transfer from "./transfer";
@@ -17,53 +17,74 @@ import * as subs from '@aws-cdk/aws-sns-subscriptions'
 import warmindDefinitions from './definitions'
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const api = new apiGateway.RestApi(this, 'warmind-gateway', {
-      restApiName: 'infra-mywarmind',
-      endpointConfiguration: { types: [EndpointType.REGIONAL] }
-    });
+        const api = new apiGateway.RestApi(this, 'warmind-gateway', {
+            restApiName: 'infra-mywarmind',
+            endpointConfiguration: {types: [EndpointType.REGIONAL]}
+        });
 
-    const bucket = new Bucket(this, 'infra-mywarmind')
-    new s3Deploy.BucketDeployment(this, 'infra-mywarmind-bucket-deploy', {
-      sources: [s3Deploy.Source.asset('../public')],
-      destinationBucket: bucket
-    })
+        const bucket = new Bucket(this, 'infra-mywarmind')
+        new s3Deploy.BucketDeployment(this, 'infra-mywarmind-bucket-deploy', {
+            sources: [s3Deploy.Source.asset('../public')],
+            destinationBucket: bucket
+        })
 
-    const role = new iam.Role(this, 'infra-mywarmind-read-s3', { assumedBy: new ServicePrincipal('s3')})
-    bucket.grantRead(role)
+        const role = new iam.Role(this, 'infra-mywarmind-read-s3', {assumedBy: new ServicePrincipal('s3')})
+        bucket.grantRead(role)
 
-    const s3Integration = new AwsIntegration( {
-      service: 's3',
-      path: 'mywarmind.net/index.html',
-      options: {
-        credentialsRole: role,
-        passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
-        integrationResponses: [{
-          statusCode: "200",
-          selectionPattern: "200",
-        }]
-      },
-    })
-    api.root.addMethod('GET', s3Integration, { methodResponses: [{ statusCode: "200" }]})
+        const s3Integration = new AwsIntegration({
+            integrationHttpMethod: "GET",
+            service: 's3',
+            path: 'mywarmind.net/index.html',
+            options: {
+                requestParameters: {
+                    "integration.request.header.Content-Type": "method.request.header.Content-Type",
+                    "integration.request.header.Content-Disposition": "method.request.header.Content-Disposition"
+                },
+                credentialsRole: role,
+                passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
+                integrationResponses: [{
+                    statusCode: "200",
+                    selectionPattern: "200",
+                    responseParameters: {
+                      "method.response.header.Content-Type" : "integration.response.header.Content-Type",
+                      "method.response.header.Content-Disposition" : "integration.response.header.Content-Disposition"
+                    }
+                }]
+            },
+        })
+        api.root.addMethod('GET', s3Integration, {
+            requestParameters: {
+                "method.request.header.Content-Type": false,
+                "method.request.header.Content-Disposition": false
+            },
+            methodResponses: [{
+                statusCode: "200",
+                responseParameters: {
+                    "method.response.header.Content-Type": false,
+                    "method.response.header.Content-Disposition": false
+                }
+            }]
+        })
 
-    const requests = LayerVersion.fromLayerVersionArn(this, 'requests', 'arn:aws:lambda:us-east-2:956931160472:layer:requests:1');
-    const xRay = LayerVersion.fromLayerVersionArn(this, 'x-ray', 'arn:aws:lambda:us-east-2:956931160472:layer:aws-xray-sdk:1');
-    const layers = [requests, xRay]
+        const requests = LayerVersion.fromLayerVersionArn(this, 'requests', 'arn:aws:lambda:us-east-2:956931160472:layer:requests:1');
+        const xRay = LayerVersion.fromLayerVersionArn(this, 'x-ray', 'arn:aws:lambda:us-east-2:956931160472:layer:aws-xray-sdk:1');
+        const layers = [requests, xRay]
 
-    auth(this, api, layers)
-    gear(this, api, layers)
-    transfer(this, api, layers)
-    weapons(this, api, layers)
-    updateManifest(this, layers)
-    general(this)
-    definitions(this)
+        auth(this, api, layers)
+        gear(this, api, layers)
+        transfer(this, api, layers)
+        weapons(this, api, layers)
+        updateManifest(this, layers)
+        general(this)
+        definitions(this)
 
-    const dynamoDbTopic = new sns.Topic(this, 'infra-mywarmind-definitions-import', {
-      displayName: 'mywarmind definitions import topic'
-    })
-    const warmindDefFn = warmindDefinitions(this, layers)
-    dynamoDbTopic.addSubscription(new subs.LambdaSubscription(warmindDefFn))
-  }
+        const dynamoDbTopic = new sns.Topic(this, 'infra-mywarmind-definitions-import', {
+            displayName: 'mywarmind definitions import topic'
+        })
+        const warmindDefFn = warmindDefinitions(this, layers)
+        dynamoDbTopic.addSubscription(new subs.LambdaSubscription(warmindDefFn))
+    }
 }
